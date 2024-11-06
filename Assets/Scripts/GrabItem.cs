99e37ea35f5ad9f53e3659ab5cee2e0f8594d0c2
JorgePastorCastilla <jordi131313@gmail.com>
2024-11-06 22:58:01 +0100

    added pickup torch script and modified .gitignore

---
+++ Assets/Scripts/GrabItem.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabItem : MonoBehaviour
+{
+    
+    public GameObject grabItem;
+    public GameObject text;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        text.SetActive(true);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                grabItem.SetActive(true);
+                text.SetActive(false);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        text.SetActive(false);
+    }
+}
