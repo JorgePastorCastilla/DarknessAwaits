8f94aff1cf195c7732bde4eec8b0343ccabfd3e4
JorgePastorCastilla <jordi131313@gmail.com>
2025-01-14 18:56:13 +0100

    added free camera roam with right click

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -87,11 +87,38 @@ public class PlayerController : MonoBehaviour
             verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
         
             camera.rotation = Quaternion.Euler(verticalRotation, horizontalRotation, 0f);
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
         else
         {
-            camera.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(_characterMovement.targetRotation), Time.deltaTime * _characterMovement.transitionRotationSpeed);
-            _characterMovement.alreadyRotating = false;
+            if (Input.GetKeyUp(KeyCode.Mouse1))
+            {
+                float cameraRotationY = camera.localEulerAngles.y;
+                Debug.Log("Camera rotation is: " + cameraRotationY);
+                if (cameraRotationY > 315 || cameraRotationY <= 45)
+                {
+                    Debug.Log("Forward");
+                    // _characterMovement.targetRotation += Vector3.up * 0;
+                }else if (cameraRotationY > 45 && cameraRotationY <= 135)
+                {
+                    Debug.Log("Right");
+                    _characterMovement.targetRotation += Vector3.up * 90;
+                }else if (cameraRotationY > 135 && cameraRotationY <= 225)
+                {
+                    Debug.Log("Backward");
+                    _characterMovement.targetRotation += Vector3.up * 180;
+                }else if (cameraRotationY > 225 && cameraRotationY <= 315)
+                {
+                    Debug.Log("Left");
+                    _characterMovement.targetRotation -= Vector3.up * 90;
+                }
+                camera.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(_characterMovement.targetRotation), Time.deltaTime * _characterMovement.transitionRotationSpeed);
+                _characterMovement.alreadyRotating = false;
+                
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
         }
     }
 
