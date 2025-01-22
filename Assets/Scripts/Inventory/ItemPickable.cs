9466c4f1f8837c764fae2dd248204565509ea4e9
JorgePastorCastilla <jordi131313@gmail.com>
2025-01-22 10:05:28 +0100

    correction on item pickup, removed trigger from pickableItem and removed pickableItem script simplified

--- Assets/Scripts/Inventory/ItemPickable.cs
+++ Assets/Scripts/Inventory/ItemPickable.cs
@@ -8,21 +8,4 @@ public class ItemPickable : MonoBehaviour
 {
     // Start is called before the first frame update
     public ItemSO itemScriptableObject;
-    public bool canPickUp = false;
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            canPickUp = true;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            canPickUp = false;
-        }
-    }
 }
